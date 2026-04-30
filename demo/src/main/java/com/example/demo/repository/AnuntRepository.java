eb7b405364e620cef2f13117d0d583bc637055c5
ItsNotMatei <mateipetru.cucuta@gmail.com>
2026-04-30 16:40:41 +0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/AnuntRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Anunt;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnuntRepository extends JpaRepository<Anunt, Long> {
-}
