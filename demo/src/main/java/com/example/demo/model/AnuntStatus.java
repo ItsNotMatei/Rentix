8f879a22acaee26fef578d857599500de39c5417
ItsNotMatei <mateipetru.cucuta@gmail.com>
2026-05-11 20:28:07 +0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/model/AnuntStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model;
-
-public enum AnuntStatus {
-    AVAILABLE,
-    RESERVED,
-    RENTED
-}
--- demo/src/main/java/com/example/demo/model/PaymentRequest.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.model;
-
-public record PaymentRequest(Long amount, Long reservationId) {}
